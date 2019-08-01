afd1f5c4d88180e17786064add7a6bf4bbdde6f9
TARS <TARS@LAPTOP-7EJ12RQB>
2019-08-01 01:24:51 -0700

    Created a reactive input system. Also tested to make sure that the new reactive input system worked.

--- src/engine/Input.java
+++ src/engine/Input.java
@@ -3,27 +3,58 @@ package engine;
 import graphics.Camera;
 import graphics.Camera.Camera2d;
 import graphics.Window;
+import java.util.ArrayList;
 import java.util.BitSet;
+import java.util.List;
 import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
 import util.math.Vec2d;
+import util.math.VectorN;
 
 public abstract class Input {
 
+    private static final List<ReactiveInputListener> R_LISTENERS = new ArrayList();
+
+    public static final int MOUSE_IN = 0;
+    public static final int KEY_IN = 1;
+    public static final int MOUSE_BUTTON_IN = 2;
+    public static final int MOUSE_WHEEL_IN = 3;
+
     static void init() {
         Window.window.setCursorPosCallback((window, xpos, ypos) -> {
-            mouse = new Vec2d(xpos / Settings.WINDOW_WIDTH, 1 - ypos / Settings.WINDOW_HEIGHT);
+            Vec2d nMouse = new Vec2d(xpos / Settings.WINDOW_WIDTH, 1 - ypos / Settings.WINDOW_HEIGHT);
+            if (!R_LISTENERS.isEmpty()) {
+                R_LISTENERS.forEach(ril -> ril.receiveGeneralInput(MOUSE_IN, nMouse, nMouse.sub(mouse), 0, false, false));
+            }
+            mouse = nMouse;
         });
         Window.window.setKeyCallback((window, key, scancode, action, mods) -> {
             if (key >= 0) {
+                boolean nks = action != GLFW_RELEASE;
+                if (!R_LISTENERS.isEmpty()) {
+                    R_LISTENERS.forEach(ril -> ril.receiveGeneralInput(KEY_IN, null, null, key, nks, nks != keys.get(key)));
+                }
                 keys.set(key, action != GLFW_RELEASE);
             }
         });
         Window.window.setMouseButtonCallback((window, button, action, mods) -> {
+            boolean nbs = action != GLFW_RELEASE;
+            if (!R_LISTENERS.isEmpty()) {
+                R_LISTENERS.forEach(ril -> ril.receiveGeneralInput(MOUSE_BUTTON_IN, mouse, null, button, nbs, nbs != buttons.get(button)));
+            }
             buttons.set(button, action != GLFW_RELEASE);
         });
         Window.window.setScrollCallback((window, xoffset, yoffset) -> {
+            Vec2d nwo = new Vec2d(xoffset, yoffset);
+            if (!R_LISTENERS.isEmpty()) {
+                R_LISTENERS.forEach(ril -> ril.receiveGeneralInput(MOUSE_WHEEL_IN, nwo, nwo.sub(vecMouseWheel), 0, false, false));
+            }
+            vecMouseWheel = nwo;
             mouseWheel = yoffset;
         });
+    }
+    
+    public static void addListener(ReactiveInputListener ril){
+        R_LISTENERS.add(ril);
     }
 
     static void nextFrame() {
@@ -43,6 +74,7 @@ public abstract class Input {
     private static BitSet prevButtons = new BitSet();
 
     private static double mouseWheel;
+    private static Vec2d vecMouseWheel = new Vec2d(0, 0);
 
     public static boolean keyDown(int key) {
         return keys.get(key);
@@ -86,5 +118,10 @@ public abstract class Input {
 
     public static double mouseWheel() {
         return mouseWheel;
+    }
+
+    public interface ReactiveInputListener {
+
+        public void receiveGeneralInput(int kind, Vec2d mouse, Vec2d deltaMouse, int key, boolean pressed, boolean changed);
     }
 }
