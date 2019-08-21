5d7f30a829252394ea6500870cc0ebf5ffb4a856
TARS <TARS@LAPTOP-7EJ12RQB>
2019-08-21 00:40:12 -0700

    Added networking code from SnowSight (OGPC-2016-Java on GitHub).

---
+++ src/network/Connection.java
@@ -0,0 +1,121 @@
+package network;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import static network.NetworkUtils.READERS;
+import static network.NetworkUtils.WRITERS;
+import util.Log;
+
+public class Connection {
+
+    private Socket socket;
+    DataInputStream input;
+    DataOutputStream output;
+
+    private boolean closed;
+    private final List<Runnable> onClose = new LinkedList();
+
+    private final Map<Byte, Runnable> handlerMap = new HashMap();
+
+    public Connection(Socket socket) {
+        this.socket = socket;
+        try {
+            input = new DataInputStream(socket.getInputStream());
+            output = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException ex) {
+            close();
+        }
+    }
+
+    public void close() {
+        closed = true;
+        try {
+            socket.close();
+        } catch (IOException ex) {
+        }
+        handlerMap.clear();
+        onClose.forEach(Runnable::run);
+        onClose.clear();
+    }
+
+    public boolean isClosed() {
+        return closed;
+    }
+
+    public void onClose(Runnable r) {
+        onClose.add(r);
+    }
+
+    public void open() {
+        new Thread(() -> {
+            while (!closed) {
+                try {
+                    byte id = input.readByte();
+                    processMessage(id);
+                } catch (IOException ex) {
+                    close();
+                }
+            }
+        }).start();
+    }
+
+    private void processMessage(byte id) {
+        if (handlerMap.containsKey(id)) {
+            handlerMap.get(id).run();
+        } else {
+            Log.error("Unknown message id: " + id + " is not one of known messages types " + handlerMap.keySet() + " of connection " + this);
+            close();
+        }
+    }
+
+    public <T> T read(Class<T> c) {
+        if (!closed) {
+            try {
+                return (T) READERS.get(c).read(this);
+            } catch (IOException ex) {
+                close();
+            }
+        }
+        return null;
+    }
+
+    public void registerHandler(int id, Runnable handler) {
+        handlerMap.put((byte) id, handler);
+    }
+
+    public synchronized void sendMessage(int id, Object... data) {
+        sendMessage(id, () -> write(data));
+    }
+
+    public synchronized void sendMessage(int id, Runnable printer) {
+        if (!closed) {
+            try {
+                output.writeByte(id);
+                printer.run();
+            } catch (IOException ex) {
+                close();
+            }
+        }
+    }
+
+    public synchronized void write(Object... a) {
+        if (!closed) {
+            try {
+                for (Object o : a) {
+                    if (!WRITERS.containsKey(o.getClass())) {
+                        throw new RuntimeException("Unknown data type: " + o.getClass());
+                    }
+                    WRITERS.get(o.getClass()).write(this, o);
+                }
+            } catch (IOException ex) {
+                close();
+            }
+        }
+    }
+}
---
+++ src/network/NetworkUtils.java
@@ -0,0 +1,105 @@
+package network;
+
+import graphics.Color;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import util.Log;
+import util.math.Vec2d;
+import util.math.Vec3d;
+
+public abstract class NetworkUtils {
+
+    //Starting and connecting to servers
+    public static final int PORT = 51243;
+
+    public static Connection connect(String ip) {
+        if (ip.equals("")) {
+            ip = "localhost";
+        }
+        try {
+            Connection c = new Connection(new Socket(ip, PORT));
+            System.out.println("Connected to server");
+            c.onClose(() -> System.out.println("Disconnected from server"));
+            return c;
+        } catch (IOException ex) {
+            System.out.println("Could not connect to server: " + ip);
+            return null;
+        }
+    }
+
+    public static Connection connectManual() {
+        System.out.println("Enter the ip address to connect to:");
+        Connection c = connect(new Scanner(System.in).nextLine());
+        if (c == null) {
+            throw new RuntimeException("Failed to connect to server");
+        }
+        return c;
+    }
+
+    public static Thread server(Consumer<Connection> onConnect) {
+        return new Thread(() -> {
+            try {
+                ServerSocket serverSocket = new ServerSocket(PORT);
+                Log.print("Server started on port " + PORT);
+                while (true) {
+                    Connection conn = new Connection(serverSocket.accept());
+                    //Going too fast somtimes means the message handlers haven't registered
+                    try {
+                        Thread.sleep(5);
+                    } catch (InterruptedException ex) {
+                        Log.error(ex);
+                    }
+                    onConnect.accept(conn);
+                }
+            } catch (IOException ex) {
+                Log.error(ex);
+            }
+        });
+    }
+
+    //Readers and writers
+    static final Map<Class, Reader<Connection, Object>> READERS = new HashMap();
+    static final Map<Class, Writer<Connection, Object>> WRITERS = new HashMap();
+
+    static {
+        registerBasicType(Boolean.class, DataInputStream::readBoolean, DataOutputStream::writeBoolean);
+        registerBasicType(Byte.class, DataInputStream::readByte, (o, b) -> o.writeByte(b));
+        registerBasicType(Float.class, DataInputStream::readFloat, DataOutputStream::writeFloat);
+        registerBasicType(Double.class, DataInputStream::readDouble, DataOutputStream::writeDouble);
+        registerBasicType(Integer.class, DataInputStream::readInt, DataOutputStream::writeInt);
+        registerBasicType(Long.class, DataInputStream::readLong, DataOutputStream::writeLong);
+        registerBasicType(String.class, i -> i.readUTF(), DataOutputStream::writeUTF);
+
+        registerType(Color.class, c -> new Color(c.read(Double.class), c.read(Double.class), c.read(Double.class), c.read(Double.class)), (c, o) -> c.write(o.r, o.g, o.b, o.a));
+        registerType(Vec2d.class, c -> new Vec2d(c.read(Double.class), c.read(Double.class)), (c, v) -> c.write(v.x, v.y));
+        registerType(Vec3d.class, c -> new Vec3d(c.read(Double.class), c.read(Double.class), c.read(Double.class)), (c, v) -> c.write(v.x, v.y, v.z));
+    }
+
+    private static <T> void registerBasicType(Class<T> c, Reader<DataInputStream, T> reader, Writer<DataOutputStream, T> writer) {
+        registerType(c, conn -> reader.read(conn.input), (conn, t) -> writer.write(conn.output, t));
+    }
+
+    public static <T> void registerType(Class<T> c, Reader<Connection, T> reader, Writer<Connection, T> writer) {
+        READERS.put(c, (Reader) reader);
+        WRITERS.put(c, (Writer) writer);
+    }
+
+    @FunctionalInterface
+    public static interface Reader<T, R> {
+
+        public R read(T t) throws IOException;
+    }
+
+    @FunctionalInterface
+    public static interface Writer<T, R> {
+
+        public void write(T t, R r) throws IOException;
+    }
+}
